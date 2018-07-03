7a038f276494f9cfeccb7c34a3b123ec4f21cb9d
Matthew Bredin <matt.bredin@gmail.com>
2018-07-03 15:10:42 -0600

    Updates to both Sta and EMU architectures for CSR. All are working

--- sparse/emuArch/csr.c
+++ sparse/emuArch/csr.c
@@ -6,226 +6,202 @@
 #include <sys/time.h>
 #include <cilk.h>
 
-#define GENERATEMAT 1
-#define GENERATEMATEXAMPLE 0
 #define SANITY 0
 
 #define NODELETS 8
+#define THREADS 4
+
+replicated long *x;
+
+void fill_x(void *ptr, long node){
+	long *gto = (long *)ptr;
+
+	for(int i = 0; i < 2000; i++)
+		gto[i] = i+1;
+}
+
+long *alloc_x(int rowM){
+	long *mr = mw_mallocrepl(rowM * sizeof(long));
+	mw_replicated_init_generic(mr, fill_x);
+
+	return mr;
+}
 
 struct timeval tval_before, tval_after, tval_result;
 
-struct Matrix{
-	long **mat;
-	char name[7];
-	int rowM;
-	int colN;
-	int matSize;
-	int nonZero;
-};
-
-
-void initMat(struct Matrix *m, int M, int N, char varName[7]){
-	m -> mat = (long **)malloc(M * sizeof(long *));
-	for(int i = 0; i < M; i++)
-		m -> mat[i] = malloc(N * sizeof(long));
-	//m -> mat = (long *)malloc(M*N*sizeof(long));
-	m -> rowM = M;
-	m -> colN = N;
-	m -> matSize = M * N;
-	for(int i = 0; i < 7; i++)
-		m -> name[i] = varName[i];
-	for(int i = 0; i < M; i++)
-		for(int j = 0; j < N; j++)
-			m -> mat[i][j] = 0;
-	#if GENERATEMAT
-		int temp = 1;
-		for(int i = 0; i < M; i++){
-			for(int j = 0; j < N; j++){
-				//*(m->mat+i*N+j) = i*N+j;
-				if(i == j){
-					m->mat[i][j] = 2;
-					temp++;
-				}
-				else if(abs(i - j) == 1){
-					m->mat[i][j] = 1;
-					temp++;
-				}
-			}
-		}
-	#endif
-	#if GENERATEMATEXAMPLE
-		m->mat[0][0] = 1;
-		m->mat[0][2] = 2;
-		m->mat[0][3] = 3;
-		m->mat[0][6] = 4;
-		m->mat[0][7] = 5;
-		m->mat[1][1] = 1;
-		m->mat[1][3] = 2;
-		m->mat[3][0] = 1;
-		m->mat[3][1] = 2;
-		m->mat[3][2] = 3;
-		m->mat[3][3] = 4;
-		m->mat[3][4] = 5;
-		m->mat[3][6] = 6;
-		m->mat[3][7] = 7;
-		m->mat[4][1] = 1;
-		m->mat[4][3] = 2;
-		m->mat[4][5] = 3;
-		m->mat[5][0] = 1;
-		m->mat[5][1] = 2;
-		m->mat[6][1] = 1;
-		m->mat[6][2] = 2;
-		m->mat[6][3] = 3;
-		m->mat[6][4] = 4;
-		m->mat[6][5] = 5;
-		m->mat[6][6] = 6;
-		m->mat[6][7] = 7;
-		m->mat[7][0] = 1;
-		m->mat[7][1] = 2;
-		m->mat[7][2] = 3;
-		m->mat[7][3] = 4;
-		m->mat[7][4] = 5;
-		m->mat[7][5] = 6;
-		m->mat[7][6] = 7;
-		m->mat[7][7] = 8;
-	#endif
-	int cnt = 0;
-	for(int i = 0; i < m->matSize; i++)
-		if(*(i + m->mat) != 0)
-			cnt++;
-
-	m -> nonZero = 	cnt;
-}
-
-void print2d(struct Matrix m){
-	for(int i = 0; i < m.matSize; i++){
-		if(i % m.rowM == 0)
-			printf("| ");
-		printf("%3ld ",*(i + m.mat));
-		if(i % m.rowM == m.rowM-1)
-			printf("|\n");
-	}
-}
-
-void printLong(long *arr, int size){
-	printf("[");
-	for(int i = 0 ; i < size; i++)
-		printf("%5ld", arr[i]);
-	printf("]");
-}
-
-void csr_comp(long *orig, long *ptr, long *x, long *csr_res, int size);
+void emuMat(long *nodeId, int rowM, int colN);
+void csr_solv(long **emuRows, long nodeRows, long colN, int i, long *loc_sol);
 
 int main(){
-	struct Matrix Orig;
-	initMat(&Orig,8,8,"Origin");
-
-	//Rearrange Matrix in memory
-	//starttiming();
+	int rowM = 2000;
+	int colN = 2000;
+	long *tempX = alloc_x(rowM);
+	mw_replicated_init((long *)&x, (long)tempX);
+	
+	long *nodes = mw_malloc1dlong(NODELETS * sizeof(long *));
+	for(int i = 0; i < NODELETS; i++)
+		nodes[i] = i;
 	/*
-	long *data = mw_malloc2d()
-	for(int i = 0; i < 100; i++){
-		if(orig[i] != 0){
-			loc_data[cnt] = orig[i];
-			loc_ind[cnt] = i;
-			cnt++;
+	for(int i = 0; i < NODELETS; i++){
+		MIGRATE(&nodes[i]);
+		printf("Node %ld: [", nodes[i]);
+		for(int j = 0; j < rowM; j++){
+			printf("%3ld", x[j]);
 		}
+		printf("End Node: %ld}\n", NODE_ID());
 	}
-	*ptr = cnt;
-
-	csr_res[rowId] = 0;
-	for(int i = 0; i < cnt; i++)
-		csr_res[rowId] += loc_data[i] * x[loc_ind[i]];
 	*/
-	
-	long *nodes = malloc(NODELETS * sizeof(long *));
-	//long *nodes = mw_malloc1dlong(NODELETS);
-	long *emuRows[Orig.rowM];
-	for(int i = 0; i < Orig.rowM; i++){
-		emuRows[i] = mw_localmalloc(Orig.colN * sizeof(long), &nodes[i%NODELETS]);
-		for(int j = 0; j < Orig.colN; j++){
-			//emuRows[i][j] = 0;
-			//if(i == j){
-			//	emuRows[i][j] = 2;
-			//}
-			//else if(abs(i - j) == 1){
-			//	emuRows[i][j] = 1;
-			//}
-			emuRows[i][j] = Orig.mat[i][j];
-		}
-	}
-	
-	
-	//Initialize the vector to be multiplied (replicated across each node)
-	long x[Orig.rowM];
-	for(int j = 0; j < Orig.rowM; j++)
-		mw_replicated_init(&x[j], j+1);
 
-	//Remotely update the row_ptr (May not need since each row is seperated and empty rows are not dropped)
-	long *csr_ptr = malloc((Orig.rowM+1) * sizeof(long));
-	csr_ptr[0] = 0;
-	//initialize Resolution Array (Update remotely from nodes)
-	long *csr_res = malloc(Orig.rowM * sizeof(long));
-
-	//Split the Original Matrix by its rows, allocating different rows to each node
-	
-	starttiming();
-	for(int i = 0; i < Orig.rowM; i++){
-		cilk_spawn csr_comp(emuRows[i], &csr_ptr[i+1], x, &csr_res[i], Orig.colN);
+	//starttiming();
+	for(int i = 0; i < NODELETS; i++){
+		cilk_spawn emuMat(&nodes[i], rowM, colN);
 	}
 	cilk_sync;
 	
-
-
-	/////////////////
-	//SpMV Solution//
-	/////////////////
-
-//	csr_sclr(csr_ptr, csr_ind, csr_data, x , csr_res, Orig.rowM);
+	
+	
 
 	//Sanity Check
 	#if SANITY
-		//print2d(Orig);
-		//printf("X = ");
-		//printf("Emu Matrix");
-		//for(int i = 0; i < Orig.rowM; i++){
-		//	printf("| ");
-		//	for(int j = 0; j < Orig.colN; j++)
-		//		printf("%3ld",emuRows[i][j]);
-		//	printf(" |\n");
-		//}
-		//printLong(x, Orig.rowM);
-		//printf("\nData = ");
-		//printLong(csr_data, Orig.nonZero);
-		//printf("\nIndex = ");
-		//printLong(csr_ind, Orig.nonZero);
-		//printf("\nPointer = ");
-		//printLong(csr_ptr, Orig.rowM+1);
-		printf("\n\nSolution:");
-		printLong(csr_res, Orig.rowM);
+		//Print Statements
 	#endif
 	//printf("Compression Time: %ld\n", compTime);
 	//printf("Execution Time: %ld\n", execTime);
 
 }
 
-void csr_comp(long *orig, long *ptr, long *x, long *csr_res, int size){
-	long cnt = 0;
-	for(int i = 0; i < size; i++)
-		if(orig[i] != 0)
-			cnt++;
-	long *loc_data = malloc(cnt * sizeof(long));
-	long *loc_ind = malloc(cnt * sizeof(long));
+void emuMat(long *nodeId, int rowM, int colN){
+	MIGRATE(&nodeId);
 	
-	for(int i = 0; i < cnt; i++){
-		if(orig[i] != 0){
-			loc_data[cnt] = orig[i];
-			loc_ind[cnt] = i;
+	////////////////////////////////////
+	/////Allocate and Define Matrix/////
+	////////////////////////////////////
+	int rowSplit = rowM / NODELETS;
+	int nodeRows = rowSplit;
+	if(*nodeId == NODELETS-1)
+		nodeRows += rowM % NODELETS;
+	long **emuRows = malloc(nodeRows * sizeof(long *));
+
+	//Allocate memory for given node
+	for(int j = 0; j < nodeRows; j++){
+		emuRows[j] = malloc(colN * sizeof(long));
+	}
+		
+	int temp = *nodeId * rowSplit;
+	for(int i = 0; i < nodeRows; i++){
+		for(int j = 0; j < colN; j++){
+			if(i+temp == j)
+				emuRows[i][j] = 2;
+			else if(abs(i+temp - j) == 1)
+				emuRows[i][j] = 1;
+			else
+				emuRows[i][j] = 0;
 		}
 	}
+	
+	//Spawn threads to run on the same node
+	/*
+	long *loc_sol = malloc(nodeRows * sizeof(long));
+	for(int i = 0; i < THREADS; i++)
+		cilk_spawn csr_solv(emuRows, nodeRows, colN, i, loc_sol);
+	cilk_sync;
+	*/
+	
+	/////////////////////////////////////
+	/////////////COMPRESSION/////////////
+	/////////////////////////////////////
+	starttiming();
+	long cnt = 0;
+	for(int i = 0; i < nodeRows; i++){
+		for(int j = 0; j < colN; j++){
+			if(emuRows[i][j] != 0){
+				cnt++;
+			}
+		}
+	}
+	long *loc_data = malloc(cnt * sizeof(long));
+	long *loc_ind = malloc(cnt * sizeof(long));
+	long *loc_ptr = malloc((nodeRows + 1) * sizeof(long));
+	loc_ptr[0] = 0;
 
-	long loc_res = 0;
-	for(int i = 0; i < cnt; i++)
-		loc_res += loc_data[i] * x[loc_ind[i]];
+	cnt = 0;
+	long pcnt = 0;
+	for(int i = 0; i < nodeRows; i++){
+		pcnt = 0;
+		for(int j = 0; j < colN; j++){
+			if(emuRows[i][j] != 0){				
+				loc_data[cnt] = emuRows[i][j];
+				loc_ind[cnt] = j;
+				cnt++;
+				pcnt++;
+			}
+		}
+		loc_ptr[i+1] = loc_ptr[i] + pcnt;
+	}
+
+	
+	//////////////////////////////////
+	/////////////SOLUTION/////////////
+	//////////////////////////////////
+	
+	long *loc_sol = malloc(nodeRows * sizeof(long));
+	for(int i = 0; i < nodeRows; i++){
+		loc_sol[i] = 0;
+		for(int j = loc_ptr[i]; j < loc_ptr[i+1]; j++){
+			loc_sol[i] += loc_data[j] * x[loc_ind[j]];
+		}
+	}	
 }
+/*
+void csr_solv(long **emuRows, long nodeRows, long colN, int threadId, long *loc_sol){
 
+	/////////////////////////////////////
+	/////////////COMPRESSION/////////////
+	/////////////////////////////////////
+	int start = threadId * (nodeRows / THREADS);
+	int stop = (threadId + 1) * (nodeRows / THREADS);
+	if(threadId == THREADS - 1)
+		stop += nodeRows % THREADS;
+	long cnt = 0;
+	
+	for(int i = start; i < stop; i++){
+		for(int j = 0; j < colN; j++){
+			if(emuRows[i][j] != 0){
+				cnt++;
+			}
+		}
+	}
+	
+	long *loc_data = malloc(cnt * sizeof(long));
+	long *loc_ind = malloc(cnt * sizeof(long));
+	long *loc_ptr = malloc((stop - start + 1) * sizeof(long));
+	loc_ptr[0] = 0;
+
+	cnt = 0;
+	long pcnt = 0;
+	for(int i = start; i < stop; i++){
+		pcnt = 0;
+		for(int j = 0; j < colN; j++){
+			if(emuRows[i][j] != 0){				
+				loc_data[cnt] = emuRows[i][j];
+				loc_ind[cnt] = j;
+				cnt++;
+				pcnt++;
+			}
+		}
+		
+		loc_ptr[i+1] = loc_ptr[i] + pcnt;
+	}
+
+	
+	//////////////////////////////////
+	/////////////SOLUTION/////////////
+	//////////////////////////////////
+	for(int i = start; i < stop; i++){
+		loc_sol[i] = 0;
+		for(int j = loc_ptr[i]; j < loc_ptr[i+1]; j++){
+			loc_sol[i] += loc_data[j] * x[loc_ind[j]];
+		}
+	}
+}
+*/
--- sparse/emuArch/csr.cdc
+++ sparse/emuArch/csr.cdc
@@ -1,5 +1,5 @@
-Post Untimed portion: CurrentRSS (MB)=10.6211
-Post Untimed portion: PeakRSS (MB)=10.6211
+Post Untimed portion: CurrentRSS (MB)=40.0938
+Post Untimed portion: PeakRSS (MB)=40.0938
 ************************************************
 Program Name/Arguments: 
 csr.mwx 
@@ -10,96 +10,12 @@ Configuration Details:
 Gossamer Cores per Nodelet=4
 Log2 Num Nodelets=3
 Log2 Memory Size/Nodelet=33
-Capture queue depths=true
-Timing sample interval (ns)=10000
+Capture queue depths=false
 Send ACKs from remotes=true
 Bandwidth multiplier through MigrEng=1
-Core Clock=300 MHz, Pd=3.333
+Core Clock=3600 MHz, Pd=0.278
 Memory DDR4-2133: Bandwidth = 1.886 GiB/s = 2.025 GB/s
 SRIO SystemIC bandwidth=2.32 GiB/s (2.5GB/s)
 ************************************************
-Post SystemC Startup: CurrentRSS (MB)=10.6211
-Post SystemC Startup: PeakRSS (MB)=10.6211
-Emu system run time 0.000141 sec==141259206 ps
-System thread counts:
-	active=4, created=8, died=4,
-	max live=5 first occurred @65690097 ps with prog 46.5% complete
-	and last occurred @65690097 ps with prog 98.1% complete
-Num_Core_Cycles=42382
-Num_SRIO_Cycles=88287
-Num_Mem_Cycles=35761
-************************************************
-MEMORY MAP
-848,0,0,0,0,0,0,0
-0,0,0,0,0,0,0,0
-0,0,0,0,0,0,0,0
-0,0,0,0,0,0,0,0
-0,0,0,0,0,0,0,0
-0,0,0,0,0,0,0,0
-0,0,0,0,0,0,0,0
-0,0,0,0,0,0,0,0
-
-************************************************
-REMOTES MAP
-0,0,0,0,0,0,0,0
-0,0,0,0,0,0,0,0
-0,0,0,0,0,0,0,0
-0,0,0,0,0,0,0,0
-0,0,0,0,0,0,0,0
-0,0,0,0,0,0,0,0
-0,0,0,0,0,0,0,0
-0,0,0,0,0,0,0,0
-
-************************************************
-Module: #_created, #_spawns, #_quits, #_migrates, #_rmos_in, #_rmos_out, mem_bw, IPC
-NLET[0]: 1, 7, 4, 0, 0, 0, 0.0317385, 0.157331
-NLET[1]: 0, 0, 0, 0, 0, 0, 0, 0
-NLET[2]: 0, 0, 0, 0, 0, 0, 0, 0
-NLET[3]: 0, 0, 0, 0, 0, 0, 0, 0
-NLET[4]: 0, 0, 0, 0, 0, 0, 0, 0
-NLET[5]: 0, 0, 0, 0, 0, 0, 0, 0
-NLET[6]: 0, 0, 0, 0, 0, 0, 0, 0
-NLET[7]: 0, 0, 0, 0, 0, 0, 0, 0
-
-Module: #_in_xacts, in_bw, #_to_nqms, #_to_sysic, p_zero, p_one, opt1, opt2, restored
-ME[0].FromNQM[0]: 0, 0, 0, 0, 0, 0, 0, 0, 0
-ME[0].FromNQM[1]: 0, 0, 0, 0, 0, 0, 0, 0, 0
-ME[0].FromNQM[2]: 0, 0, 0, 0, 0, 0, 0, 0, 0
-ME[0].FromNQM[3]: 0, 0, 0, 0, 0, 0, 0, 0, 0
-ME[0].FromNQM[4]: 0, 0, 0, 0, 0, 0, 0, 0, 0
-ME[0].FromNQM[5]: 0, 0, 0, 0, 0, 0, 0, 0, 0
-ME[0].FromNQM[6]: 0, 0, 0, 0, 0, 0, 0, 0, 0
-ME[0].FromNQM[7]: 0, 0, 0, 0, 0, 0, 0, 0, 0
-
-Module: #_in_xacts, in_bw, out_bw, passers_by, passers_stored
-ME[0].FromSysIC[0]: 0, 0, 0, 0, 0
-ME[0].FromSysIC[1]: 0, 0, 0, 0, 0
-ME[0].FromSysIC[2]: 0, 0, 0, 0, 0
-ME[0].FromSysIC[3]: 0, 0, 0, 0, 0
-ME[0].FromSysIC[4]: 0, 0, 0, 0, 0
-ME[0].FromSysIC[5]: 0, 0, 0, 0, 0
-
-Module: #_out_xacts, out_bw, avg_total_reqs, avg_total_reqs_exZero, avg_rio_reqs, avg_rio_reqs_exZero, avg_nqm_reqs, avg_nqm_reqs_exZero
-ME[0].ToNQM[0]: 0, 0, 0, -nan, 0, -nan, 0, -nan
-ME[0].ToNQM[1]: 0, 0, 0, -nan, 0, -nan, 0, -nan
-ME[0].ToNQM[2]: 0, 0, 0, -nan, 0, -nan, 0, -nan
-ME[0].ToNQM[3]: 0, 0, 0, -nan, 0, -nan, 0, -nan
-ME[0].ToNQM[4]: 0, 0, 0, -nan, 0, -nan, 0, -nan
-ME[0].ToNQM[5]: 0, 0, 0, -nan, 0, -nan, 0, -nan
-ME[0].ToNQM[6]: 0, 0, 0, -nan, 0, -nan, 0, -nan
-ME[0].ToNQM[7]: 0, 0, 0, -nan, 0, -nan, 0, -nan
-
-Module: #_out_xacts, out_bw, avg_total_reqs, avg_total_reqs_exZero, avg_rio_reqs, avg_rio_reqs_exZero, avg_nqm_reqs, avg_nqm_reqs_exZero
-ME[0].ToSysIC[0]: 0, 0, 0, -nan, 0, -nan, 0, -nan
-ME[0].ToSysIC[1]: 0, 0, 0, -nan, 0, -nan, 0, -nan
-ME[0].ToSysIC[2]: 0, 0, 0, -nan, 0, -nan, 0, -nan
-ME[0].ToSysIC[3]: 0, 0, 0, -nan, 0, -nan, 0, -nan
-ME[0].ToSysIC[4]: 0, 0, 0, -nan, 0, -nan, 0, -nan
-ME[0].ToSysIC[5]: 0, 0, 0, -nan, 0, -nan, 0, -nan
-
-
-************************************************
-End of simulation: CurrentRSS (MB)=10.6211
-End of simulation: PeakRSS (MB)=10.6211
-************************************************
-Simulator wall clock time (seconds): 0
+Post SystemC Startup: CurrentRSS (MB)=40.0938
+Post SystemC Startup: PeakRSS (MB)=40.0938
